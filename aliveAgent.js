81689c1820481a5f7e8612d4e2508bd9ddc8ffc4
Anton Drachev <anton@drachev.com>
2013-03-18 17:06:41 +0400

    real keepalive in aliveAgent.js

--- aliveAgent.js
+++ aliveAgent.js
@@ -24,7 +24,9 @@ function Agent(options) {
   self.options = options || {};
   self.requests = {};
   self.sockets = {};
+  self.idleSockets = {};
   self.maxSockets = self.options.maxSockets || Agent.defaultMaxSockets;
+  self.keepAlive = self.options.keepAlive || Agent.defaultKeepAlive;
   self.on('free', function(socket, host, port, localAddress) {
     var name = host + ':' + port;
     if (localAddress) {
@@ -43,7 +45,8 @@ function Agent(options) {
       // socket and it will get removed from the pool. This
       // gets us out of timeout issues and allows us to
       // default to Connection:keep-alive.
-      socket.destroy();
+      self.removeFromSockets(socket, name);
+      self.putIdleSocket(socket, name);
     }
   });
   self.createConnection = net.createConnection;
@@ -52,6 +55,7 @@ util.inherits(Agent, EventEmitter);
 exports.Agent = Agent;
 
 Agent.defaultMaxSockets = 5;
+Agent.defaultKeepAlive = 10 * 1000;
 
 Agent.prototype.defaultPort = 80;
 Agent.prototype.addRequest = function(req, host, port, localAddress) {
@@ -64,7 +68,7 @@ Agent.prototype.addRequest = function(req, host, port, localAddress) {
   }
   if (this.sockets[name].length < this.maxSockets) {
     // If we are under maxSockets create a new one.
-    req.onSocket(this.createSocket(name, host, port, localAddress, req));
+    req.onSocket(this.findOrCreateSocket(name, host, port, localAddress, req));
   } else {
     // We are over limit so we'll add it to the queue.
     if (!this.requests[name]) {
@@ -73,7 +77,11 @@ Agent.prototype.addRequest = function(req, host, port, localAddress) {
     this.requests[name].push(req);
   }
 };
-Agent.prototype.createSocket = function(name, host, port, localAddress, req) {
+Agent.prototype.findOrCreateSocket = function(name, host, port, localAddress, req) {
+  var idleSocket = this.findIdleSocket(name);
+  if (idleSocket) {
+    return idleSocket;
+  }
   var self = this;
   var options = util._extend({}, self.options);
   options.port = port;
@@ -117,6 +125,53 @@ Agent.prototype.createSocket = function(name, host, port, localAddress, req) {
   return s;
 };
 Agent.prototype.removeSocket = function(s, name, host, port, localAddress) {
+  this.removeFromSockets(s, name);
+  this.removeFromIdleSockets(s, name);
+
+  if (this.requests[name] && this.requests[name].length) {
+    var req = this.requests[name][0];
+    // If we have pending requests and a socket gets closed a new one
+    this.findOrCreateSocket(name, host, port, localAddress, req).emit('free');
+  }
+};
+Agent.prototype.putIdleSocket = function(socket, name) {
+  if (!this.idleSockets[name]) {
+    this.idleSockets[name] = [];
+  }
+  this.idleSockets[name].push({ socket: socket, validTill: +new Date() + this.keepAlive });
+  this.setupValidator();
+};
+Agent.prototype.setupValidator = function() {
+  if ("validationTimer" in this) {
+    return;
+  }
+  var self = this;
+  this.validationTimer = setTimeout(function() {
+    delete self.validationTimer;
+    var socketsLeft = 0;
+    for (var name in self.idleSockets) if (self.idleSockets.hasOwnProperty(name)) {
+      socketsLeft += self.validateIdle(name);
+    }
+    if (socketsLeft > 0) {
+      self.setupValidator();
+    }
+  }, Math.max(this.keepAlive / 10, 1000));
+};
+Agent.prototype.findIdleSocket = function(name) {
+  if (!this.idleSockets[name] || !this.validateIdle(name)) {
+    return null;
+  }
+  var sobj = this.idleSockets[name].shift();
+  if (!sobj) {
+    return null;
+  }
+  if (!this.sockets[name]) {
+    this.sockets[name] = [];
+  }
+  this.sockets[name].push(sobj.socket);
+  return sobj.socket;
+};
+Agent.prototype.removeFromSockets = function(s, name) {
   if (this.sockets[name]) {
     var index = this.sockets[name].indexOf(s);
     if (index !== -1) {
@@ -127,9 +182,46 @@ Agent.prototype.removeSocket = function(s, name, host, port, localAddress) {
       }
     }
   }
-  if (this.requests[name] && this.requests[name].length) {
-    var req = this.requests[name][0];
-    // If we have pending requests and a socket gets closed a new one
-    this.createSocket(name, host, port, localAddress, req).emit('free');
+}
+Agent.prototype.removeFromIdleSockets = function(s, name) {
+  if (this.idleSockets[name]) {
+    var index = -1;
+    this.idleSockets[name].some(function(sobj, i) {
+      if (sobj.socket === s) {
+        index = i;
+        return true;
+      }
+      return false;
+    });
+    if (index !== -1) {
+      this.idleSockets[name].splice(index, 1);
+      if (this.idleSockets[name].length === 0) {
+         // don't leak
+         delete this.idleSockets[name];
+      }
+    }
   }
+};
+Agent.prototype.validateIdle = function(name) {
+  var sockets = this.idleSockets[name];
+  var now = +new Date();
+  var count = 0;
+  sockets.some(function(sobj, i) {
+    if (sockets[i].validTill < now) {
+      count++;
+      return false;
+    }
+    return true;
+  });
+  if (count > 0) {
+    var removed = sockets.splice(0, count);
+    removed.forEach(function(sobj) {
+      sobj.socket.destroy();
+    });
+    if (sockets.length === 0) {
+      // don't leak
+      delete this.idleSockets[name];
+    }
+  }
+  return sockets.length > 0;
 };
